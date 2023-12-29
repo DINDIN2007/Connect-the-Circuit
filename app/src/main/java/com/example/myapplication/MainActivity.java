b91f76fcd6ef30aeb451735db69c64df890a6408
Dinh Viet Luong <dinhvietluong99@gmail.com>
2023-12-29 15:45:47 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static int counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void launchSetting(View v) {
+        // launch a new activity
+        Intent i = new Intent(this, SettingsActivity.class);
+        i.putExtra("COOL", "Hello");
+        startActivity(i);
+    }
+}
